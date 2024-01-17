e409eab28279603f4ce7b7b61ff89bdf83b43a64
NaveenYadav <yn.naveen00@gmail.com>
2024-01-18 02:34:06 +0530

    Component added to page and public images downloaded

--- app/page.js
+++ app/page.js
@@ -1,11 +1,22 @@
 import Image from 'next/image'
 import styles from './page.module.css'
+import Card from '@/stories/Card/Card'
+import Form from '@/stories/Form/Form'
+import Table from '@/stories/Table/Table'
+import Page from '@/stories/Page/Page'
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <h1>Hello</h1>
-      <h1>naveen</h1>
+      <div className="container">
+        <Form />
+        <br /><br />
+        <Card />
+        <br /><br />
+        <Table />
+        <br /><br />
+        <Page />
+      </div>
     </main>
   )
 }
