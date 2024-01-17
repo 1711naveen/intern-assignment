ccc9a4da061c1872e3d92213b2d925d77f9f636d
NaveenYadav <yn.naveen00@gmail.com>
2024-01-18 02:44:00 +0530

    page to normal

--- app/page.js
+++ app/page.js
@@ -1,11 +1,21 @@
-import Navbar from "./Component/Navbar";
-
+import Image from 'next/image'
+import styles from './page.module.css'
+import Card from '@/stories/Card/Card'
+import Form from '@/stories/Form/Form'
+import Table from '@/stories/Table/Table'
+import Page from '@/stories/Page/Page'
 
 export default function Home() {
   return (
 
     <div className="container">
-      <Navbar/>
+      <Form />
+      <br /><br />
+      <Card />
+      <br /><br />
+      <Table />
+      <br /><br />
+      <Page />
     </div>
 
   )
