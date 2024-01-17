b2b4815bd45daad7ea6911a3c0fb6a285cffa4da
NaveenYadav <yn.naveen00@gmail.com>
2024-01-18 02:40:16 +0530

    Added Navbar to page

--- app/page.js
+++ app/page.js
@@ -1,21 +1,11 @@
-import Image from 'next/image'
-import styles from './page.module.css'
-import Card from '@/stories/Card/Card'
-import Form from '@/stories/Form/Form'
-import Table from '@/stories/Table/Table'
-import Page from '@/stories/Page/Page'
+import Navbar from "./Component/Navbar";
+
 
 export default function Home() {
   return (
 
     <div className="container">
-      <Form />
-      <br /><br />
-      <Card />
-      <br /><br />
-      <Table />
-      <br /><br />
-      <Page />
+      <Navbar/>
     </div>
 
   )
