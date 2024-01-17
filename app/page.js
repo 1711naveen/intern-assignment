407ae51397a0d4bd8bcfed8e6bf669f3b8dd8e11
NaveenYadav <yn.naveen00@gmail.com>
2024-01-18 02:36:17 +0530

    page changed

--- app/page.js
+++ app/page.js
@@ -7,16 +7,16 @@ import Page from '@/stories/Page/Page'
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className="container">
-        <Form />
-        <br /><br />
-        <Card />
-        <br /><br />
-        <Table />
-        <br /><br />
-        <Page />
-      </div>
-    </main>
+
+    <div className="container">
+      <Form />
+      <br /><br />
+      <Card />
+      <br /><br />
+      <Table />
+      <br /><br />
+      <Page />
+    </div>
+
   )
 }
